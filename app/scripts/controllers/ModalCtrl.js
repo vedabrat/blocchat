32c91145ad3235478ec667ff09930b0cc5d0f7e5
vedabrat <vedabrat>
2017-09-06 23:12:22 -0400

    cookie?

--- app/scripts/controllers/ModalCtrl.js
+++ app/scripts/controllers/ModalCtrl.js
@@ -8,13 +8,14 @@
              Room.add(this.newRoom);
              $uibModalInstance.close();
          };
-         this.setUsername = function () {
-            Room.add(this.newRoom);
+         this.setUsername = function (currentUser) {
+            $cookies.put('blocChatCurrentUser', currentUser);
+            console.log('I SAVED A COOKIE IN PART 2');
             $uibModalInstance.close();
          };
      }
 
      angular
          .module('blocChat')
-         .controller('ModalCtrl', ['Room', '$uibModalInstance', ModalCtrl]);
+         .controller('ModalCtrl', ['Room', '$uibModalInstance', ModalCtrl, '$cookies']);
  })();
--- app/scripts/services/BlocChatCookies.js
+++ app/scripts/services/BlocChatCookies.js
@@ -1,25 +1,18 @@
 (function() {
   function BlocChatCookies($cookies) {
     var currentUser = $cookies.get('blocChatCurrentUser');
-    var user = {};
-    this.username = user.username;
-    this.email =user.email;
-    this.password = user.password;
 
     console.log($cookies.get('blocChatCurrentUser'));
     console.log(!currentUser);
-    console.log(currentUser === '');
-    // if (!currentUser || currentUser === '') {
-
-          currentUser = prompt("What is your username");
-    //       while (currentUser.username = null){}
-    //         prompt("give me a username. if you dont this box will never leave")
-    // }
-    // $cookieStore(currentUser);
-    // console.log($cookies.get(blocChatCurrentUser));
-    // if (username = '')
-
+    console.log(currentUser);
+    if (!currentUser || currentUser === '') {
+      currentUser = prompt("give me currentUser NOW.");
+      $cookies.put('blocChatCurrentUser', currentUser);
+      console.log('I SAVED A COOKIE IN PART 1');
+      console.log($cookies.get('blocChatCurrentUser'));
     }
+    console.log(currentUser);
+  }
 
 
 
